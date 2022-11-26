e289248f6a2ff8fa96e63f84c102bfbdc5654f88
veyseltosun <postalarim1923@gmail.com>
2022-11-26 07:53:24 +0300

    dispatch .

--- redux/src/components/ComponentA.jsx
+++ redux/src/components/ComponentA.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "../App.css"
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 
 function ComponentA() {
     const myCounter = useSelector((state) => state.counter)
+    const dispatch = useDispatch()
   return (
     <div className='App' style={{backgroundColor:"yellow", height:"35vh"}}>
         ComponentA
         <h2>Count:{myCounter} </h2>
-        <button>Artır</button>
-        <button>Eksilt</button>
+        <button onClick={() => dispatch({type:"INCREASE_COUNTER"})}>Artır</button>
+        <button onClick={() => dispatch({type:"DECREASE_COUNTER"})}>Eksilt</button>
         
     </div>
   )
