04c6dd20155f277f660d78d55071c7ec3dea8266
veyseltosun <postalarim1923@gmail.com>
2022-11-26 08:21:23 +0300

    actionType .

--- redux/src/redux/reducer.js
+++ redux/src/redux/reducer.js
@@ -1,12 +1,15 @@
+import {INCREASE_COUNTER, DECREASE_COUNTER, RESET_COUNTER} from "./actionTypes";
+
+
 function reducer (state,action) {
     switch (action.type){
-        case "INCREASE_COUNTER":
+        case INCREASE_COUNTER:
             state.counter = state.counter+1;
             return {...state};
-        case "DECREASE_COUNTER" :
+        case DECREASE_COUNTER :
             state.counter = state.counter-1;
             return {...state};
-        case "RESET_COUNTER" :
+        case RESET_COUNTER :
             state.counter = action.payload;
             return {...state};
 
